66bff75691549436de8bb33b828feeab97485e54
Shahriyor <shaxriyors553@gmail.com>
2023-12-22 16:11:57 +0500

    style

--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -1,8 +1,25 @@
 import "./style.scss"
+import { Link, Outlet } from 'react-router-dom';
 
 function Navigation() {
     return (
-        <h1>Navigation</h1>
+        <div>
+            <div className="navbar">
+                <div className="navbar-left">
+                    <h1>🪁</h1>
+                </div>
+                <div className="navbar-middle"></div>
+                <div className="navbar-links">
+                    <Link to="/about">Главная</Link>
+                    <Link to="/about">Экскурсии</Link>
+                    <Link to="/about">О Личный кабинет</Link>
+
+                </div>
+                <div id='outlet'>
+                    <Outlet />
+                </div>
+            </div>
+        </div>
     )
 }
 
